3b3ed9b903f6da49739aa1392a52457923f1b419
Admir Durmishi <durmisi@gmail.com>
2024-03-28 21:41:14 +0100

    Add project files.

---
+++ NetCoreAnalyzers.Sample/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using NetCoreAnalyzers.Sample.Models;
+using System.Diagnostics;
+
+namespace NetCoreAnalyzers.Sample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public ActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
